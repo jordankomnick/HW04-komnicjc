06f4fe155bfacf4170d555753217a619c32f6813
Jordan Komnick <jordan@komnick.com>
2012-10-26 13:15:42 -0400

    it compiles, builds, and runs now

--- vc10/komnicjcStarbucks.cpp
+++ vc10/komnicjcStarbucks.cpp
@@ -1,3 +1,7 @@
-class komnicjcStarbucks : public Starbucks { 
+#include "Starbucks.h"
+#include "komnicjcStarbucks.h"
+
 /* member variables and methods declared here */ 
-};
+	
+	void build(Entry* c, int n);
+	Entry* getNearest(double x, double y);
--- vc10/komnicjcStarbucks.h
+++ vc10/komnicjcStarbucks.h
@@ -4,15 +4,7 @@
 using namespace std;
 
 class komnicjcStarbucks : public Starbucks 
-{ 
-	class Entry 
-	{
-		public:
-		string identifier;
-		double x;
-		double y;
-	};
-
+{
 	virtual void build(Entry* c, int n);
 	virtual Entry* getNearest(double x, double y);
 
