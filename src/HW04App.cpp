f74f700c14acf6474fca55cc77a0f26d114c3eb3
Jordan Komnick <jordan@komnick.com>
2012-10-28 20:47:59 -0400

    add all entries to an array

--- src/HW04App.cpp
+++ src/HW04App.cpp
@@ -1,7 +1,12 @@
+//some file i/o referenced from http://www.cplusplus.com/doc/tutorial/files/ and https://github.com/Sonodabe/HW04_Sonodabe
+
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/gl.h"
+#include "Starbucks.h"
+#include "komnicjcStarbucks.h"
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace ci;
 using namespace ci::app;
@@ -13,24 +18,46 @@ class HW04App : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+	Entry* locations;
 };
 
 void HW04App::setup()
 {
+	int count = 0;
 	string line;
+	double num;
 	ifstream myfile;
 	myfile.open("../include/Starbucks_2006.csv");
 	if (myfile.is_open())
-  {
-    /*while ( myfile.good() )
-    {*/
-      getline (myfile,line);
-      cout << line << endl;
-    /*}*/
-    myfile.close();
-  }
-
+	{
+		while ( myfile.good() )
+		{
+		  getline (myfile, line);
+		  count++;
+		}
+		myfile.close();
+	}
   else cout << "Unable to open file";
+
+  locations = new Entry[count];
+
+  myfile.open("../include/Starbucks_2006.csv");
+	if (myfile.is_open())
+	{
+		while ( myfile.good() )
+		{
+			Entry* temp_entry = new Entry();
+		  getline (myfile, line, ',');
+		  temp_entry -> identifier = line;
+		  myfile >> temp_entry -> x;
+		  myfile.get();
+		  myfile >> temp_entry -> y;
+		  myfile.get();
+		  count++;
+		}
+		myfile.close();
+	}
+	else cout << "Unable to open file";
 }
 
 void HW04App::mouseDown( MouseEvent event )
